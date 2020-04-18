11147f7a7f943c271b1ebf3612facbbe0314d79c
Dmytro Shvaika <shvajka@gmail.com>
2020-04-18 16:58:06 +0300

    WIP: set missing busy times

--- src/routes/get-busy-times.js
+++ src/routes/get-busy-times.js
@@ -1,116 +1,148 @@
 import geoTz from 'geo-tz'
 import moment from 'moment-timezone'
 import axios from 'axios'
+import { logger } from '../libs/index.js'
+import { dump } from '../utils/index.js'
 import { Location } from '../models/index.js'
 
 const { INTERSECTION_DELTA_MINUTES } = process.env
 
-export default async (req, res) => {
-  const { placeId } = req.params
+const timeRanges = ['9am - 12pm', '12pm - 3pm', '3pm - 6pm', '6pm - 9pm']
 
-  const { data } = await axios({
-    method: 'GET',
-    url: `http://overpass-api.de/api/interpreter?data=[out:json];way(${placeId});out geom;`,
-  })
-  const placePolygon = data.elements[0].geometry
-    .reduce((polygon, coordinates) => {
-      polygon.coordinates[0].push([coordinates.lon, coordinates.lat])
-      return polygon
-    }, {
-      type: 'Polygon',
-      coordinates: [[]],
+const fillMissingTimeRanges = busyHours => busyHours
+  .map(busyHour => {
+    busyHour.timeRange = busyHour.timeRange || []
+
+    busyHour.timeRange.sort(
+      (a, b) => timeRanges.findIndex(r => r === a.timeRange) > timeRanges.findIndex(r => r === b.timeRange) ? 1 : -1,
+    )
+
+    timeRanges.forEach((range, index) => {
+      if (!busyHour.timeRange[index] || busyHour.timeRange[index].timeRange !== range) {
+        busyHour.timeRange.splice(index, 0, { timeRange: range, count: 0 })
+      }
     })
 
-  const timezone = geoTz(placePolygon.coordinates[0][0][1], placePolygon.coordinates[0][0][0])[0]
+    return busyHour
+  })
+  .sort((a, b) => a.dayOfWeek - b.dayOfWeek)
 
-  const busyHours = await Location.aggregate([{
-    $match: {
-      $and: [{
-        createdAt: {
-          $gt: moment().tz(timezone).subtract(6, 'month').toDate(),
-        },
+export default async (req, res) => {
+  res.setHeader('Content-Type', 'application/json')
+  try {
+    const { placeId } = req.params
+
+    const { data } = await axios({
+      method: 'GET',
+      url: `http://overpass-api.de/api/interpreter?data=[out:json];way(${placeId});out geom;`,
+    })
+    const placePolygon = data.elements[0].geometry
+      .reduce((polygon, coordinates) => {
+        polygon.coordinates[0].push([coordinates.lon, coordinates.lat])
+        return polygon
       }, {
-        location: {
-          $geoWithin: {
-            $geometry: placePolygon,
+        type: 'Polygon',
+        coordinates: [[]],
+      })
+
+    const timezone = geoTz(placePolygon.coordinates[0][0][1], placePolygon.coordinates[0][0][0])[0]
+
+    const busyHours = await Location.aggregate([{
+      $match: {
+        $and: [{
+          createdAt: {
+            $gt: moment().tz(timezone).subtract(6, 'month').toDate(),
+          },
+        }, {
+          location: {
+            $geoWithin: {
+              $geometry: placePolygon,
+            },
+          },
+        }],
+      },
+    }, {
+      $project: {
+        device: true,
+        dayOfWeek: {
+          $dayOfWeek: {
+            date: '$createdAt',
+            timezone,
           },
         },
-      }],
-    },
-  }, {
-    $project: {
-      device: true,
-      dayOfWeek: {
-        $dayOfWeek: {
-          date: '$createdAt',
-          timezone,
+        timeRange: {
+          $concat: [
+            { $cond: [{ $lt: [{ $hour: { date: '$createdAt', timezone } },9] }, 'Unknown', ''] },
+            { $cond: [{ $and: [{ $gte: [{ $hour: { date: '$createdAt', timezone } }, 9] }, { $lt: [{ $hour: { date: '$createdAt', timezone } }, 12] }] }, '9am - 12pm', ''] },
+            { $cond: [{ $and: [{ $gte: [{ $hour: { date: '$createdAt', timezone } }, 12] }, { $lt: [{ $hour: { date: '$createdAt', timezone } }, 15] }] }, '12pm - 3pm', ''] },
+            { $cond: [{ $and: [{ $gte: [{ $hour: { date: '$createdAt', timezone } }, 15] }, { $lt: [{ $hour: { date: '$createdAt', timezone } }, 18] }] }, '3pm - 6pm', ''] },
+            { $cond: [{ $and: [{ $gte: [{ $hour: { date: '$createdAt', timezone } }, 18] }, { $lt: [{ $hour: { date: '$createdAt', timezone } }, 21] }] }, '6pm - 9pm', ''] },
+            { $cond: [{ $gte: [{ $hour: { date: '$createdAt', timezone } },21] }, 'Unknown', ''] },
+          ],
         },
       },
-      timeRange: {
-        $concat: [
-          { $cond: [{ $lt: [{ $hour: '$createdAt' },9] }, 'Unknown', ''] },
-          { $cond: [{ $and: [{ $gte: [{ $hour: '$createdAt' }, 9] }, { $lt: [{ $hour: '$createdAt' }, 12] }] }, '9am-12pm', ''] },
-          { $cond: [{ $and: [{ $gte: [{ $hour: '$createdAt' }, 12] }, { $lt: [{ $hour: '$createdAt' }, 15] }] }, '12pm-3pm', ''] },
-          { $cond: [{ $and: [{ $gte: [{ $hour: '$createdAt' }, 15] }, { $lt: [{ $hour: '$createdAt' }, 18] }] }, '3pm-6pm', ''] },
-          { $cond: [{ $and: [{ $gte: [{ $hour: '$createdAt' }, 18] }, { $lt: [{ $hour: '$createdAt' }, 21] }] }, '6pm - 9pm', ''] },
-          { $cond: [{ $gte: [{ $hour: '$createdAt' },21] }, 'Unknown', ''] },
-        ],
+    }, {
+      $group: {
+        _id: {
+          dayOfWeek: '$dayOfWeek',
+          timeRange: '$timeRange',
+        },
+        count: { $sum: 1 },
       },
-    },
-  }, {
-    $group: {
-      _id: {
-        dayOfWeek: '$dayOfWeek',
+    }, {
+      $match: {
+        '_id.timeRange': { $ne: 'Unknown' },
+      },
+    }, {
+      $group: {
+        _id: '$_id.dayOfWeek',
+        timeRange: { $push: { timeRange: '$_id.timeRange', count: '$count' } },
+      },
+    }, {
+      $project: {
+        _id: false,
+        dayOfWeek: '$_id',
         timeRange: '$timeRange',
       },
-      count: { $sum: 1 },
-    },
-  }, {
-    $project: {
-      _id: false,
-      dayOfWeek: '$_id.dayOfWeek',
-      timeRange: '$_id.timeRange',
-      count: true,
-    },
-  }, {
-    $match: {
-      timeRange: { $ne: 'Unknown' },
-    },
-  }])
+    }])
 
-  const [{ count: devicesAtPlaceNow }] = await Location.aggregate([{
-    $match: {
-      $and: [{
-        createdAt: {
-          $gt: moment().tz(timezone).subtract(INTERSECTION_DELTA_MINUTES, 'minutes').toDate(),
-        },
-      }, {
-        location: {
-          $geoWithin: {
-            $geometry: placePolygon,
+    const [{ count: devicesAtPlaceNow }] = await Location.aggregate([{
+      $match: {
+        $and: [{
+          createdAt: {
+            $gt: moment().tz(timezone).subtract(INTERSECTION_DELTA_MINUTES, 'minutes').toDate(),
           },
-        },
-      }],
-    },
-  }, {
-    $group: {
-      _id: '$device',
-    },
-  }, {
-    $group: {
-      _id: null,
-      count: { $sum: 1 },
-    },
-  }, {
-    $project: {
-      _id: false,
-      count: true,
-    },
-  }])
+        }, {
+          location: {
+            $geoWithin: {
+              $geometry: placePolygon,
+            },
+          },
+        }],
+      },
+    }, {
+      $group: {
+        _id: '$device',
+      },
+    }, {
+      $group: {
+        _id: null,
+        count: { $sum: 1 },
+      },
+    }, {
+      $project: {
+        _id: false,
+        count: true,
+      },
+    }])
 
-  console.dir({ devicesAtPlaceNow }, { depth: 20, colors: true })
-  console.dir({ busyHours }, { depth: 20, colors: true })
+    return res.json({
+      devicesAtPlaceNow,
+      busyHours: fillMissingTimeRanges(busyHours),
+    })
+  } catch (err) {
+    logger.error(err)
 
-
-  return res.json({})
+    return res.status(err.statusCode || 502).send(dump.dumpError(err))
+  }
 }
